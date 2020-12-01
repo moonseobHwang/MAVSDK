11584897063dd4fe9fb5f7181084d85c606515ec
moon <aa6271235@naver.com>
2020-12-01 10:03:00 +0900

    'add Takeoff(Continue)'

--- examples/takeoff_land/connect_result.cpp
+++ examples/takeoff_land/connect_result.cpp
@@ -13,7 +13,7 @@ using namespace std::chrono;
 
 
 int main(int argc, char** argv) {
-// connect_result @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// connect_result @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     Mavsdk mavsdk; ConnectionResult connection_result;
     bool discovered_system = false;
     connection_result = mavsdk.add_any_connection(argv[1]);
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
         [](ComponentType component_type) -> void { std::cout << unsigned(component_type) ; }
     ); // Register a callback components (camera, gimbal) etc are found
 
-// Regist_Telemetry @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+// Regist_Telemetry @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     auto telemetry = std::make_shared<Telemetry>(system);
     // We want to listen to the altitude of the drone at 1 Hz.
     const Telemetry::Result set_rate_result = telemetry->set_rate_position(1.0);
@@ -48,6 +48,14 @@ int main(int argc, char** argv) {
     const Action::Result arm_result = action->arm();
     if (arm_result != Action::Result::Success) {
         std::cout << "Arming failed:" << arm_result << std::endl;
+        return 1;
+    }
+
+// Takeoff(Continue) @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    std::cout << "Taking off..." << std::endl;
+    const Action::Result takeoff_result = action->takeoff();
+    if (takeoff_result != Action::Result::Success) {
+        std::cout << "Takeoff failed:" << takeoff_result << std::endl;
         return 1;
     }
     return 0;
